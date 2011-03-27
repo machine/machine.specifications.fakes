962769b0c07ea523e45844fea3984b00d3adeb46
Bjoern Rochel <bjoern@bjro.de>
2011-03-27 18:33:27 +0100

    Added inline constraints for FakeItEasy. (currently failing)

--- Source/Machine.Fakes.Adapters.FakeItEasy/FakeItEasyEngine.cs
+++ Source/Machine.Fakes.Adapters.FakeItEasy/FakeItEasyEngine.cs
@@ -55,7 +55,9 @@ namespace Machine.Fakes.Adapters.FakeItEasy
 
         public TParam Match<TParam>(Expression<Func<TParam, bool>> matchExpression)
         {
-            throw new NotSupportedException("Inline constraints are currently not supported in Machine.Fakes API. Use the FakeItEasy built-in mechanism instead!");
+            var matcher = matchExpression.Compile();
+
+            return A<TParam>.That.Matches(matcher);
         }
     }
 }
