0dadb2a05182758f7fd54080812f5c1f5b68ecb3
Bjoern Rochel <bjoern@bjro.de>
2011-04-15 12:03:29 +0200

    Added spec for factories in a Registrar

---
+++ Source/Machine.Fakes.Adapters.Specs/RegistrarSpecs/Using_registrars_and_factories.cs
@@ -0,0 +1,36 @@
+using Machine.Fakes.Adapters.Rhinomocks;
+using Machine.Fakes.Sdk;
+using Machine.Specifications;
+
+namespace Machine.Fakes.Adapters.Specs.RegistrarSpecs
+{
+    [Subject(typeof (SpecificationController<>))]
+    public class When_a_factory_is_specified_in_a_registrar : WithSubject<Configuration, RhinoFakeEngine>
+    {
+        Establish context = () =>
+        {
+            Use(c => c.For<IConfigurationStore>().Use(() => new ConfigurationStore()));
+        };
+
+        Because of = () => { };
+
+        It should_use_the_factory_to_create_the_dependency = 
+            () => Subject.ConfigurationStore.ShouldBeOfType<ConfigurationStore>();
+    }
+
+    public class Configuration
+    {
+        public IConfigurationStore ConfigurationStore;
+
+        public Configuration(IConfigurationStore configurationStore)
+        {
+            ConfigurationStore = configurationStore;
+        }
+    }
+
+    public interface IConfigurationStore {}
+
+    class ConfigurationStore : IConfigurationStore
+    {
+    }
+}
