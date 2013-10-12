d7b528c2a0053c634755c43f66a113b912388bda
Simon Hohenadl <simon@hohenadl.de>
2013-10-13 00:26:07 +0200

    Updated to the latest FAKE version

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@
 :Build
 cls
 
-"Source\.nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "Source\packages" "-Version" "1.74.257.0" "-ExcludeVersion"
+"Source\.nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "Source\packages" "-Version" "1.74.269.0" "-ExcludeVersion"
 
 SET TARGET="Default"
 IF NOT [%1]==[] (set TARGET="%1")
