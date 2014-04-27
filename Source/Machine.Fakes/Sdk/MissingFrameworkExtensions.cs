ef72b9231c27f862a068c8ec7cfeaa2687f1c566
Simon Hohenadl <simon@hohenadl.de>
2014-04-27 18:36:04 +0200

    Copied Each method from MSpec; removed unused code

---
+++ Source/Machine.Fakes/Sdk/MissingFrameworkExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Machine.Fakes.Sdk
+{
+    internal static class MissingFrameworkExtensions
+    {
+        public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (T obj in enumerable)
+                action(obj);
+        }
+    }
+}
--- Source/Machine.Fakes/Sdk/ReflectionExtensions.cs
+++ Source/Machine.Fakes/Sdk/ReflectionExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
-using Machine.Specifications.Utility;
+using Machine.Fakes.Internal;
 
 namespace Machine.Fakes.Sdk
 {
