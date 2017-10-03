239e80b9df2806eec659ca5eb116e74026d25ad9
Robert Coltheart <robert.coltheart@gmail.com>
2017-10-03 21:48:22 +0800

    [Bug] Fix build and FakeItEasy rewriter

---
+++ Source/Machine.Fakes.FakeItEasy/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace Machine.Fakes.Adapters.FakeItEasy
+{
+    public static class TypeExtensions
+    {
+        public static bool IsValueType(this Type type)
+        {
+#if NET40
+            return type.IsValueType;
+#else
+            return type.GetTypeInfo().IsValueType;
+#endif
+        }
+    }
+}
