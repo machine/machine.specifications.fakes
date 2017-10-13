734d2f2617a4900dc5907e399d60df9797ef98de
Robert Coltheart <robert.coltheart@gmail.com>
2017-10-13 20:49:44 +0800

    fix local build

--- build-local.bat
+++ build-local.bat
@@ -1,1 +1,1 @@
-  powershell -f build.ps1 -Configuration Debug -CodeDirectory "Source" -TestsDirectory "Source" -PackageOutputDirectory "Build" -Package "Machine.Fakes","Machine.Fakes.FakeItEasy","Machine.Fakes.Moq","Machine.Fakes.NSubstitute","Machine.Fakes.Rhinomocks"
+  powershell -f build.ps1 -Configuration Debug -CodeDirectory "Source" -TestsDirectory "Source" -PackageOutputDirectory "Build"
